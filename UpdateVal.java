ecc0b8911f2200dacd6b213ff946a1cb06b74094
Shradha Manure <118459173+Shraddhamanure555@users.noreply.github.com>
2025-03-09 13:02:38 +0530

    UpdateVal.java

--- UpdateVal.java
+++ UpdateVal.java
@@ -2,4 +2,26 @@
 operators(==,+=,-=,*=,/=,%=) and display the intermediate result*/
 public class UpdateVal {
 
+	public static void main(String args[]) {
+		int a = 88;
+		System.out.println("oringial value of a: "+ a);
+	//	+=
+		a += 7;
+		System.out.println("PLUS equal to: "+a);
+	//  -=
+		a-=5;
+		System.out.println("substract equal to: "+ a);
+	//  *=
+		a*=2;
+		System.out.println("multiply equal to: "+ a);
+	//.../=
+		a/=3;
+		System.out.println("Division equal to: " + a);
+	//  %=
+		a%=2;
+		System.out.println("Modulus equal to: "+ a);
+		
+		
+		
+	}
 }
