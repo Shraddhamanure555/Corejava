6432a89513879bdb2f390a13ba98d6ff1a0452ff
Shradha Manure <118459173+Shraddhamanure555@users.noreply.github.com>
2025-03-10 11:45:25 +0530

    GreaterThan.java

--- GreaterThan.java
+++ GreaterThan.java
@@ -1,4 +1,11 @@
 //Write a java program to check whether a given no is grater than 100 or not using relational ope.
 public class GreaterThan {
-
+public static void main(String args[]){
+  int a =101;
+  if(a > 100){
+    System.out.println("No is greater than 100");
+  }else{
+    System.out.println("No is less than 100");
+  }
+}
 }
